a517e834d73c3b40077d1ac0a8fdceeb16b59c86
Stephanie Loomer <sdloomer@gmail.com>
2023-06-20 12:11:42 -0700

    finished ERD, table schemas, queries

---
+++ EmployeeSQL/Table Schemas.sql
@@ -0,0 +1,51 @@
+﻿-- Create Tables for all six CSV files --
+CREATE TABLE titles (
+    title_id VARCHAR(30) PRIMARY KEY,
+    title VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE employees (
+    emp_no INT PRIMARY KEY,
+    emp_title_id VARCHAR(30) NOT NULL,
+    birth_date VARCHAR(30) NOT NULL,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    sex VARCHAR(5) NOT NULL,
+    hire_date VARCHAR(30) NOT NULL,
+	FOREIGN KEY (emp_title_id) REFERENCES titles(title_id)
+);
+
+CREATE TABLE departments (
+    dept_no VARCHAR(30) PRIMARY KEY,
+    dept_name VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE dept_emp (
+    emp_no INT NOT NULL,
+    dept_no VARCHAR(30) NOT NULL,
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
+	FOREIGN KEY (dept_no) REFERENCES departments(dept_no)
+);
+
+CREATE TABLE dept_manager (
+    dept_no VARCHAR(30),
+    emp_no INT NOT NULL,
+	FOREIGN KEY (dept_no) REFERENCES departments(dept_no),
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
+);
+
+CREATE TABLE salaries (
+    emp_no INT NOT NULL,
+    salary INT NOT NULL,
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
+);
+
+-- Import CSV files in order of creation --
+
+-- View all tables to make sure data was imported correctly --
+SELECT * FROM titles
+SELECT * FROM departments
+SELECT * FROM employees
+SELECT * FROM dept_emp
+SELECT * FROM dept_manager
+SELECT * FROM salaries
